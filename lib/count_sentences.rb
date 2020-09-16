6a49adbae41f9df982ec1dbae4ec2a1ef1a18b2b
kylefarmer85 <kylefarmer85@gmail.com>
2020-09-16 18:53:35 -0500

    finished lab

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,20 +1,37 @@
 require 'pry'
 
 class String
+  attr_accessor :counter
+
+  def initialize
+    @counter = 0
+  end
 
   def sentence?
-    
+    if self.end_with?(".")
+      return true
+    else
+      return false
+    end
   end
 
   def question?
-
+    if self.end_with?("?")
+      return true
+    else
+      return false
+    end
   end
 
   def exclamation?
-
+    if self.end_with?("!")
+      return true
+    else
+      return false
+    end
   end
 
   def count_sentences
-
+    self.split(/[.!?] /).count
   end
 end
